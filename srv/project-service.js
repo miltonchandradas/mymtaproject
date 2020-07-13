311fa3d53165ca11f8a9b27de0e14526525b8d43
Milton Chandradas <milton.chandradas@sap.com>
2020-07-13 04:18:15 +0000

    custom headers...

--- srv/project-service.js
+++ srv/project-service.js
@@ -8,6 +8,14 @@ module.exports = srv => {
         console.log(`Target: ${req.target.name}`.yellow.inverse);
     });
 
+    srv.before("READ", "Users", async req => {
+        const customHeader = req._.req.headers["customheader"];
+        console.log(customHeader);
+
+        if (customHeader !== "abracadabra") 
+            req.reject(403, "Forbidden...  Secret phrase is incorrect");
+    })
+
     srv.before("CREATE", "Users", async req => {
         console.log("CREATE User...");
         console.log(req.data);
